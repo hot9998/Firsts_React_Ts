e571c36f8644c09d4999e93bf104088a4dfa8f99
ChangHyunkmov <hot9998@kmov.kr>
2020-07-04 18:51:08 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import Grettings from "./Greetings";
-import Counter from "./Counter";
-import MyForm from "./MyForm";
+// import Grettings from "./Greetings";
+// import Counter from "./Counter";
+// import MyForm from "./MyForm";
 import ReducerSample from "./ReducerSample";
 
 function App() {
-  const onClick = (name: string) => {
-    console.log(`${name} says Hello`);
-  };
-  const onSubmit = (form: { name: string; description: string }) => {
-    console.log(form);
-  };
+  // const onClick = (name: string) => {
+  //   console.log(`${name} says Hello`);
+  // };
+  // const onSubmit = (form: { name: string; description: string }) => {
+  //   console.log(form);
+  // };
 
   return (
     <div className="App">
